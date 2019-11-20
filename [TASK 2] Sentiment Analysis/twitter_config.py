f8d219b9cd8c9f035163f467078eee551df404fb
Joseph <fruitfuljoseph@gmail.com>
2019-11-20 19:22:53 +0530

    Update Readme

--- [TASK 2] Sentiment Analysis/twitter_config.py
+++ [TASK 2] Sentiment Analysis/twitter_config.py
@@ -1,7 +1,11 @@
 #These information comes from Twitter API
 #Create a Twitter Account and Get These Information from apps.twitter.com
 
-consumer_key = 'exFpGOeTIYfwIN2ZdHHfDzG2y'
-consumer_secret = 'by0X5jyZXrawJn82uuGY9a8ymJ0YffezakJ9FhNO95GsxsTkdx'
-access_token = '3317958877-KbuPexMO5vVVWa6EKsgh4fWkxP2K7CqRI4Fhrql'
-access_secret = 'i5UGN5T4zePg1D7xfTRVLyFgPKAI6wD1k3eAaJUDcast9'
+#Create your own Twitter API keys and paste them here
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
+
+
